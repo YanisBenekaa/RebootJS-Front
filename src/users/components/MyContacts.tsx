8afcf774b2717961dff826f62d359eaf138c19da
Yanis Benekaa <yanis.benekaa@hotmail.fr>
2020-09-10 15:05:00 +0200

    Add Button with Link

--- src/users/components/MyContacts.tsx
+++ src/users/components/MyContacts.tsx
@@ -1,35 +1,49 @@
-import React from 'react';
-import ContactListItem from './ContactListItem';
-import { User } from '../types';
-import { getUsers } from '../../api/methods';
-import { List, ListItem, Button } from '@material-ui/core';
+import React from "react";
+import ContactListItem from "./ContactListItem";
+import { User } from "../types";
+import { getUsers } from "../../api/methods";
+import { List, ListItem, Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 interface ContactListState {
-    users: User[];
+  users: User[];
 }
 
-class ContactList extends React.Component<{}, ContactListState>{
-    constructor(props: {}) {
-        super(props)
-        this.state = {
-            users: []
-        }
-    }
+class ContactList extends React.Component<{}, ContactListState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      users: [],
+    };
+  }
 
-    componentDidMount() {
-        getUsers().then(fetchedUsers => { this.setState({ users: fetchedUsers }) })
-    }
+  componentDidMount() {
+    getUsers().then((fetchedUsers) => {
+      this.setState({ users: fetchedUsers });
+    });
+  }
 
-    render() {
-        return <div>
-            <h1>Liste de contact</h1>
-            <List>
-                {this.state.users.map((user) => <ListItem><ContactListItem firstname={user.firstname} lastname={user.lastname} /></ListItem>)}
-            </List>
+  render() {
+    return (
+      <div>
+        <h1>Liste de contact</h1>
+        <List>
+          {this.state.users.map((user) => (
+            <ListItem>
+              <ContactListItem
+                firstname={user.firstname}
+                lastname={user.lastname}
+              />
+            </ListItem>
+          ))}
+        </List>
 
-            <Button color="primary">Chat</Button>
-        </div>
-    }
+        <Button color="primary">
+          <Link to="/login">Se Connecter</Link>
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default ContactList;
