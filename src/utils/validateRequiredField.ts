7e77a712bde15dd976647dcae379290824d2fa49
Yanis Benekaa <yanis.benekaa@hotmail.fr>
2020-09-10 15:12:11 +0200

    Add validateRequiredField function

---
+++ src/utils/validateRequiredField.ts
@@ -0,0 +1,10 @@
+export function validateRequiredField(value: string): boolean {
+  // if(!value){
+  //   return false
+  // } else {
+  //   return true
+  // }
+
+  // return value ? true : false;
+  return !!value;
+}
