33017978eb8eea6b97d4541e66470a97a12ca7b4
Yanis Benekaa <yanis.benekaa@hotmail.fr>
2020-09-10 14:48:18 +0200

    Fix Axios with credentials

--- src/api/methods.tsx
+++ src/api/methods.tsx
@@ -1,10 +1,25 @@
 import { User } from "../users/types";
-import axios from 'axios';
+import axios from "axios";
+import { IProfile } from "../profile/types";
 
 // fetch users via the server
 export function getUsers(): Promise<User[]> {
-    return axios.get('http://localhost:3000/profile')
-        .then(resp => {
-            return resp.data
-        })
+  return axios.get("http://localhost:3000/profile").then((resp) => {
+    return resp.data;
+  });
+}
+
+export function login(email: string, password: string): Promise<IProfile> {
+  return axios
+    .post(
+      "http://localhost:3000/login",
+      {
+        username: email,
+        password: password,
+      },
+      {
+        withCredentials: true,
+      }
+    )
+    .then((resp) => resp.data);
 }
