be61b3c841e9438cb0a4a440159da250301892df
Yanis Benekaa <yanis.benekaa@hotmail.fr>
2020-09-11 11:40:50 +0200

    Add IFormPasswordField and defaultFasswordField

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,8 +1,36 @@
 export interface IFormField {
   value: string;
   isValid: boolean;
+  error?: string;
+}
+
+export interface IProfileFormFields {
+  email: IFormField;
+  password: IFormPasswordField;
+  confirmation: IFormField;
+  firstname: IFormField;
+  lastname: IFormField;
+}
+
+export interface IFormPasswordField extends IFormField {
+  hasLower: boolean;
+  hasUpper: boolean;
+  hasNumber: boolean;
+  hasSymbol: boolean;
+  hasValidLength: boolean;
 }
 
 export function defaultFormField(): IFormField {
   return { value: "", isValid: true };
+}
+
+export function defaultPasswordField(): IFormPasswordField {
+  return {
+    ...defaultFormField(),
+    hasLower: false,
+    hasUpper: false,
+    hasNumber: false,
+    hasSymbol: false,
+    hasValidLength: false,
+  };
 }
