bbc9710a0563ed331d6d1ea29add04ce9113de31
Yanis Benekaa <yanis.benekaa@hotmail.fr>
2020-09-11 11:38:13 +0200

    Add react backend env

--- src/api/methods.tsx
+++ src/api/methods.tsx
@@ -4,7 +4,7 @@ import { IProfile } from "../profile/types";
 
 // fetch users via the server
 export function getUsers(): Promise<User[]> {
-  return axios.get("http://localhost:3000/profile").then((resp) => {
+  return axios.get(`${process.env.REACT_APP_BACKEND}/profile`).then((resp) => {
     return resp.data;
   });
 }
@@ -12,7 +12,7 @@ export function getUsers(): Promise<User[]> {
 export function login(email: string, password: string): Promise<IProfile> {
   return axios
     .post(
-      "http://localhost:3000/login",
+      `${process.env.REACT_APP_BACKEND}/login`,
       {
         username: email,
         password: password,
@@ -21,5 +21,21 @@ export function login(email: string, password: string): Promise<IProfile> {
         withCredentials: true,
       }
     )
+    .then((resp) => resp.data);
+}
+
+export function register(
+  email: string,
+  password: string,
+  firstname: string,
+  lastname: string
+): Promise<IProfile> {
+  return axios
+    .post(`${process.env.REACT_APP_BACKEND}/profile`, {
+      email,
+      password,
+      firstname,
+      lastname,
+    })
     .then((resp) => resp.data);
 }
