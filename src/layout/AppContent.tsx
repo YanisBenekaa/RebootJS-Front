74ffdf03e2353dc7c15eb518f9656c5bfb4b18da
Yanis Benekaa <yanis.benekaa@hotmail.fr>
2020-09-13 14:14:10 +0200

    Add Profile route

--- src/layout/AppContent.tsx
+++ src/layout/AppContent.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import MyContacts from '../users/components/MyContacts';
-import LoginScreen from '../login/components/LoginScreen';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import MyContacts from "../users/components/MyContacts";
+import LoginScreen from "../login/components/LoginScreen";
+import MyProfile from "../profile/components/MyProfile";
 
 class AppContent extends React.Component {
-    render() {
-        return (
-            <Switch>
-                <Route path="/login"><LoginScreen /></Route>
-                <Route path="/" component={MyContacts}></Route>
-            </Switch>
-        )
-    }
+  render() {
+    return (
+      <Switch>
+        <Route path="/profile" component={MyProfile} />
+        <Route path="/login" component={LoginScreen} />
+        <Route path="/" component={MyContacts} />
+      </Switch>
+    );
+  }
 }
 
 export default AppContent;
