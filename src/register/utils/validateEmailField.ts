64f54ba828d2b27bc9f06b816ccb067902325535
Yanis Benekaa <yanis.benekaa@hotmail.fr>
2020-09-13 14:29:04 +0200

    Update validateEmailField

--- src/register/utils/validateEmailField.ts
+++ src/register/utils/validateEmailField.ts
@@ -4,5 +4,7 @@ export function validateEmailField(email: IFormField) {
   email.isValid = /^[a-z0-9-._]+@[a-z0-9-._]+\.[a-z]{2,}$/gi.test(email.value);
   if (!email.isValid) {
     email.error = "Le format de l'adresse mail est invalide";
+  } else {
+    delete email.error;
   }
 }
