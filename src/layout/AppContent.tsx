c49c1636eb070a6c681c4115e064cdf4461dbcf6
Yanis Benekaa <yanis.benekaa@hotmail.fr>
2020-09-15 12:01:50 +0200

    Update AppContent

--- src/layout/AppContent.tsx
+++ src/layout/AppContent.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import ContactList from "../users/components/ContactList";
 import LoginScreen from "../login/components/LoginScreen";
 import MyProfile from "../profile/components/MyProfile";
+import { HomeScreen } from "./HomeScreen";
 
 class AppContent extends React.Component {
   render() {
@@ -10,7 +10,7 @@ class AppContent extends React.Component {
       <Switch>
         <Route path="/profile" component={MyProfile} />
         <Route path="/login" component={LoginScreen} />
-        <Route path="/" component={ContactList} />
+        <Route path="/" component={HomeScreen} />
       </Switch>
     );
   }
