b12b27a20291957ac6290dc2bd7e8e3e645c358b
Yanis Benekaa <yanis.benekaa@hotmail.fr>
2020-09-15 17:02:05 +0200

    Add route for conversation

--- src/layout/AppContent.tsx
+++ src/layout/AppContent.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import ChatUI from "../conversations/components/ChatUI";
 import LoginScreen from "../login/components/LoginScreen";
 import MyProfile from "../profile/components/MyProfile";
 import { HomeScreen } from "./HomeScreen";
@@ -8,6 +9,7 @@ class AppContent extends React.Component {
   render() {
     return (
       <Switch>
+        <Route path="/conversation/:conversationId" component={ChatUI} />
         <Route path="/profile" component={MyProfile} />
         <Route path="/login" component={LoginScreen} />
         <Route path="/" component={HomeScreen} />
