e6fdf5dd70c1ef5ddaa6fa7134e4e879b7b29e40
Yanis Benekaa <yanis.benekaa@hotmail.fr>
2020-09-15 11:48:52 +0200

    Add access to user list in ConversationListItem

--- src/users/components/ContactList.tsx
+++ src/users/components/ContactList.tsx
@@ -1,34 +1,20 @@
 import React from "react";
 import ContactListItem from "./ContactListItem";
 import { User } from "../types";
-import { getUsers } from "../../api/methods";
 import { List, ListItem, Button } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
-interface ContactListState {
+interface ContactListProps {
   users: User[];
 }
 
-class ContactList extends React.Component<{}, ContactListState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      users: [],
-    };
-  }
-
-  componentDidMount() {
-    getUsers().then((fetchedUsers) => {
-      this.setState({ users: fetchedUsers });
-    });
-  }
-
+class ContactList extends React.Component<ContactListProps> {
   render() {
     return (
       <div>
         <h1>Liste de contact</h1>
         <List>
-          {this.state.users.map((user, index) => (
+          {this.props.users.map((user, index) => (
             <ListItem key={index}>
               <ContactListItem
                 firstname={user.firstname}
