2461ae70df5e0c105be6629e6c2152f39e1c63b5
Yanis Benekaa <yanis.benekaa@hotmail.fr>
2020-09-20 11:32:55 +0200

    Update types

--- src/profile/types.ts
+++ src/profile/types.ts
@@ -2,4 +2,19 @@ export interface IProfile {
   email: string;
   firstname: string;
   lastname: string;
+  conversationSeen: { [conversationId: string]: string };
 }
+
+// --- Définition des types de l'actions ---
+export const UPDATE_CONNECTED_PROFILE = "UPDATE_CONNECTED_PROFILE";
+export interface UpdateConnectedProfileAction {
+  type: typeof UPDATE_CONNECTED_PROFILE;
+  profile: IProfile;
+}
+// ---
+
+export interface IProfileState {
+  connectedProfile?: IProfile;
+}
+
+export type IProfileAction = UpdateConnectedProfileAction;
