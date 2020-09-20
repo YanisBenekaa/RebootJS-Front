ac9676092ab5945f6f767abb7bd52b764bad5cda
Yanis Benekaa <yanis.benekaa@hotmail.fr>
2020-09-20 12:06:16 +0200

    Add Redux for Menu

--- src/layout/components/AppMenu.tsx
+++ src/layout/components/AppMenu.tsx
@@ -4,16 +4,21 @@ import Forum from "@material-ui/icons/Forum";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import React, { Fragment } from "react";
-import { ProfileButton } from "../ProfileButton";
+import { ProfileButton } from "./ProfileButton";
 import { IDrawerContent } from "../types";
 import { Contacts, ForumSharp } from "@material-ui/icons";
 import { IconButton } from "@material-ui/core";
+import { IAppState } from "../../appReducer";
+import { connect } from "react-redux";
+import { IProfile } from "../../profile/types";
+import { changeDrawerContent } from "../actions/changeDrawerContentAction";
 
 interface AppMenuProps {
   changeDrawerContent: (content: IDrawerContent) => void;
+  profile?: IProfile;
 }
 
-export function AppMenu({ changeDrawerContent }: AppMenuProps) {
+export function AppMenu({ changeDrawerContent, profile }: AppMenuProps) {
   return (
     <Fragment>
       <AppBar position="static" style={{ height: "10vh" }}>
@@ -29,11 +34,15 @@ export function AppMenu({ changeDrawerContent }: AppMenuProps) {
               <Typography variant="h3">flint.</Typography>
             </Toolbar>
           </Grid>
-          <Grid item>
-            <Toolbar>
-              <h1>Nom de l'utilisateur</h1>
-            </Toolbar>
-          </Grid>
+          {profile ? (
+            <Grid item>
+              <Toolbar>
+                <h1>
+                  {profile.firstname} {profile.lastname}
+                </h1>
+              </Toolbar>
+            </Grid>
+          ) : null}
           <Grid item>
             <Toolbar>
               <IconButton
@@ -57,4 +66,13 @@ export function AppMenu({ changeDrawerContent }: AppMenuProps) {
   );
 }
 
-export default AppMenu;
+const mapStateToProps = ({ profile }: IAppState) => ({
+  profile: profile.connectedProfile,
+});
+
+const mapDispatchToProps = (dispatch: any) => ({
+  changeDrawerContent: (content: IDrawerContent) =>
+    dispatch(changeDrawerContent(content)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AppMenu);
