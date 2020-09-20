d65ff6e74c07b8f23e29677a13c6ecb55c718b02
Yanis Benekaa <yanis.benekaa@hotmail.fr>
2020-09-20 11:51:34 +0200

    Update types file

--- src/layout/types.ts
+++ src/layout/types.ts
@@ -1,1 +1,14 @@
 export type IDrawerContent = "contacts" | "conversations";
+
+export const CHANGE_DRAWER_CONTENT = "CHANGE_DRAWER_CONTENT";
+export interface IChangeDrawerContentAction {
+  type: typeof CHANGE_DRAWER_CONTENT;
+  content?: IDrawerContent;
+  showDrawer: boolean;
+}
+export interface ILayoutState {
+  drawerContent?: IDrawerContent;
+  showDrawer: boolean;
+}
+
+export type ILayoutAction = IChangeDrawerContentAction;
