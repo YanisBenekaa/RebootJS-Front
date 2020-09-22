74b4d0ae44ff8cd7b0c6d6e6eee296e901f027c2
Yanis Benekaa <yanis.benekaa@hotmail.fr>
2020-09-22 14:50:56 +0200

    Update interfaces

--- src/profile/types.ts
+++ src/profile/types.ts
@@ -1,4 +1,5 @@
 export interface IProfile {
+  _id: string;
   email: string;
   firstname: string;
   lastname: string;
@@ -7,14 +8,26 @@ export interface IProfile {
 
 // --- Définition des types de l'actions ---
 export const UPDATE_CONNECTED_PROFILE = "UPDATE_CONNECTED_PROFILE";
+export const UPDATE_PROFILE_LIST = "UPDATE_PROFILE_LIST";
+// ------
+
+// Définition des actions
 export interface UpdateConnectedProfileAction {
   type: typeof UPDATE_CONNECTED_PROFILE;
   profile: IProfile;
+}
+
+export interface UpdateProfileListAction {
+  type: typeof UPDATE_PROFILE_LIST;
+  profiles: IProfile[];
 }
 // ---
 
 export interface IProfileState {
   connectedProfile?: IProfile;
+  list: IProfile[];
 }
 
-export type IProfileAction = UpdateConnectedProfileAction;
+export type IProfileAction =
+  | UpdateConnectedProfileAction
+  | UpdateProfileListAction;
