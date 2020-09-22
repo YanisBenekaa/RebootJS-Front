66d55225867154aa3e1bd4efbbeb4ffb700bad8e
Yanis Benekaa <yanis.benekaa@hotmail.fr>
2020-09-22 14:52:01 +0200

    Update profile reducer file

--- src/profile/reducer.ts
+++ src/profile/reducer.ts
@@ -1,8 +1,10 @@
 import { updateConnectedProfileCase } from "./cases/updateConnectedProfileCase";
+import { updateProfileListCase } from "./cases/updateProfileListCase";
 import {
   IProfileAction,
   IProfileState,
   UPDATE_CONNECTED_PROFILE,
+  UPDATE_PROFILE_LIST,
 } from "./types";
 import { defaultProfileState } from "./utils/defaultProfileState";
 
@@ -13,6 +15,8 @@ export function profile(
   switch (action.type) {
     case UPDATE_CONNECTED_PROFILE:
       return updateConnectedProfileCase(state, action);
+    case UPDATE_PROFILE_LIST:
+      return updateProfileListCase(state, action);
     default:
       return state;
   }
