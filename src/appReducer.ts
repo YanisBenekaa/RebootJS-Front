4cc7377d50eae2a6a410c69adce055cd17520d34
Yanis Benekaa <yanis.benekaa@hotmail.fr>
2020-09-23 15:38:08 +0200

    Add conversation to appReducer

--- src/appReducer.ts
+++ src/appReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import { profile } from "./profile/reducer";
 import { layout } from "./layout/reducer";
+import { conversation } from "./conversations/reducer";
 
 export const appReducer = combineReducers({
   profile,
   layout,
+  conversation,
 });
 
 export type IAppState = ReturnType<typeof appReducer>;
