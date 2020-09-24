7cdafc7bd429ea977ed96d481df086bbb40b14cd
Yanis Benekaa <yanis.benekaa@hotmail.fr>
2020-09-24 17:27:09 +0200

    Update index with initialization files

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { store } from "./store";
+import { makeInitApp } from "./layout/actions/makeInitApp";
+import { Action } from "redux";
+import { ThunkDispatch } from "redux-thunk";
+import { IAppState } from "./appReducer";
+
+(store.dispatch as ThunkDispatch<IAppState, void, Action>)(makeInitApp());
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
